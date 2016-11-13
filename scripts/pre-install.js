1794692e3e04bd32df79962e5c925c21f81d3fe1
coolchem <varun8686@gmail.com>
2016-11-12 17:04:03 -0700

    testing heroku

--- scripts/pre-install.js
+++ scripts/pre-install.js
@@ -3,15 +3,5 @@
 
 if(process.env.NODE_ENV === "production")
 {
-    const exec = require('child_process').execSync;
-
-/*    exec("ln -s /usr/local/heroku/bin/heroku /usr/bin/heroku");*/
-    try {
-        exec("/usr/bin/heroku config:set NPM_CONFIG_PRODUCTION=false");
-    }
-    catch (e){
-        exec("/usr/local/heroku/bin/heroku config:set NPM_CONFIG_PRODUCTION=false");
-    }
-
-
+    process.env.NPM_CONFIG_PRODUCTION=false;
 }
